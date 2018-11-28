62781b854871d1ac8835afe9b362861c30772e72
Doug Enas <douglassenas@gmail.com>
2018-11-28 12:16:23 -0500

    Updates

---
+++ precache-manifest.3d67db6f61b44495fd3958ce0b388740.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb5d57de38284f0a3d03",
+    "url": "/todos-part-3/static/js/runtime~main.eb5d57de.js"
+  },
+  {
+    "revision": "7320c3d6b8e2c2c3d53d",
+    "url": "/todos-part-3/static/js/main.7320c3d6.chunk.js"
+  },
+  {
+    "revision": "9067e5323a2ebf28f0a8",
+    "url": "/todos-part-3/static/js/1.9067e532.chunk.js"
+  },
+  {
+    "revision": "7320c3d6b8e2c2c3d53d",
+    "url": "/todos-part-3/static/css/main.9aaf8adf.chunk.css"
+  },
+  {
+    "revision": "e87d456457e793eacab3d548fa568d60",
+    "url": "/todos-part-3/index.html"
+  }
+];
